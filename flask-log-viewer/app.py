e853d7d50ca9c77542b97b2b340b138f140f4c1b
madlalakhaya25 <144236210+madlalakhaya25@users.noreply.github.com>
2023-12-18 12:15:25 +0200

    modified files and added files

--- flask-log-viewer/app.py
+++ flask-log-viewer/app.py
@@ -1,68 +1,107 @@
 from flask import Flask, render_template, request
 import re
+import json
 
 app = Flask(__name__)
 
 
-def get_log_entries(log_file_path):
-    with open(log_file_path, 'r') as file:
-        # Read the content of the log file and split it into a list of entries
-        logs = file.read().split('\n')
+# Classify log entries based on content
+def classify_log_entry(entry):
+    entry_lower = entry.lower()
+    if 'critical' in entry_lower:
+        return 'critical'
+    elif 'error' in entry_lower:
+        return 'error'
+    elif 'warning' in entry_lower:
+        return 'warning'
+    elif 'notice' in entry_lower:
+        return 'notice'
+    elif 'debug' in entry_lower:
+        return 'debug'
+    elif 'info' in entry_lower:
+        return 'info'
+    return 'normal'
 
-        # Process logs to replace Unicode characters
-        processed_logs = [log.replace('\u003c', '<').replace('\u003e', '>') for log in logs]
-
-        return processed_logs
-
-
-def highlight_entries(log_entries, keyword=None):
-    highlighted_entries = []
-
+# Process and classify log entries
+def process_log_entries(log_entries):
+    processed_entries = []
     for entry in log_entries:
-        # Highlight search keyword if provided and present in the entry
-        if keyword and keyword.lower() in entry.lower():
-            entry = re.sub(f'({re.escape(keyword)})', r'<span class="highlight">\1</span>', entry, flags=re.IGNORECASE)
-
-        # Highlight ERROR entries
-        elif 'ERROR' in entry:
-            entry = f'<span class="error">{entry}</span>'
-
-        # Highlight WARNING entries
-        elif 'WARNING' in entry:
-            entry = f'<span class="warning">{entry}</span>'
-
-        # Highlight INFO entries containing "Redis is up"
-        elif 'INFO' in entry and 'Redis is up' in entry:
-            entry = f'<span class="redis-up">{entry}</span>'
-
-        # Highlight INFO entries for deployed bots
-        elif 'INFO' in entry and 'deployed_bots' in entry:
-            entry = f'<span class="deployed-bots">{entry}</span>'
-
-        highlighted_entries.append(entry)
-
-    return highlighted_entries
+        # Assign the 'class' property based on content
+        if 'error' in entry['content'].lower():
+            entry['class'] = 'error'
+        elif 'warning' in entry['content'].lower():
+            entry['class'] = 'warning'
+        else:
+            entry['class'] = 'normal'
+        
+        # Append the processed entry to the result list
+        processed_entries.append(entry)
+    
+    return processed_entries
 
 
+# Route to handle search functionality
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    # Execute search and display results
+    search_keyword = request.form.get('search', '') if request.method == 'POST' else ''
+    log_entries = get_log_entries('path_to_log_file')
+    highlighted_entries = highlight_entries(log_entries, keywords=[search_keyword])
+    return render_template('log_viewer.html', log_entries=highlighted_entries, search_keyword=search_keyword)
+
+# Retrieve and process log entries from a file
+def get_log_entries(log_file_path):
+    try:
+        with open(log_file_path, 'r') as file:
+            log_entries = [{
+                "content": log.replace('<', '&lt;').replace('>', '&gt;'),
+                "class": classify_log_entry(log)
+            } for log in file.read().split('\n') if log.strip()]  # Ensure we don't process empty lines
+            return log_entries
+    except FileNotFoundError:
+        return [{"content": "Error: Log file not found.", "class": "error"}]
+    except Exception as e:
+        return [{"content": f"Error reading log file: {e}", "class": "error"}]
+
+
+# Highlight keywords in log entries
+def highlight_keyword(entry, keyword):
+    # Case-insensitive highlighting of keyword in entry
+    entry_lower = entry.lower()
+    keyword_lower = keyword.lower()
+    if keyword_lower in entry_lower:
+        # Wrap the matched keyword with HTML <span> tags for highlighting
+        return entry_lower.replace(keyword_lower, f'<span class="highlight">{keyword_lower}</span>')
+    else:
+        return entry  # No keyword found, return the original entry
+
+# Highlight a keyword within a log entry
+def highlight_entries(log_entries, keywords):
+    return [{"content": highlight_keyword(entry, keyword), "class": classify_log_entry(entry)}
+            for entry in log_entries for keyword in keywords if keyword]
+
+# Main route to display the log viewer
 @app.route('/')
-def index():
-    # Path to the log file
+def log_viewer():
     log_file_path = 'logfile.log'
-
-    # Get log entries from the file
     log_entries = get_log_entries(log_file_path)
-
-    # Get the search keyword from the query parameters
     keyword = request.args.get('search', '')
-
-    print(f'Search Keyword: {keyword}')  # For debugging
-
-    # Highlight log entries based on the presence of errors or warnings
-    log_entries = highlight_entries(log_entries, keyword)
-
-    # Render the log viewer template with the processed log entries
+    log_entries = highlight_entries(log_entries, keywords=[keyword]) if keyword else log_entries
     return render_template('log_viewer.html', log_entries=log_entries, search_keyword=keyword)
 
+# Route to display all log entries
+@app.route('/all')
+def all_entries():
+    log_entries = get_log_entries('logfile.log')
+    return render_template('log_viewer.html', log_entries=log_entries)
+
+# Custom filter for pretty printing JSON
+@app.template_filter('to_pretty_json')
+def to_pretty_json_filter(s):
+    try:
+        return json.dumps(json.loads(s), indent=4, sort_keys=True)
+    except ValueError:
+        return s
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Log Viewer Application
+
+## Description
+This application is a Flask-based web app designed to display and search through log files. It highlights various log levels and allows for keyword searching within the logs.
+
+## Features
+- View log entries with highlighted log levels (Info, Warning, Error, etc.).
+- Search functionality for filtering log entries.
+- Responsive design for various screen sizes.
+
+## Technology Stack
+- Flask (Python web framework)
+- HTML/CSS for frontend
+- JavaScript for client-side interactions
+
+## Setup and Installation
+1. Ensure Python and Flask are installed on your system.
+2. Clone the repository to your local machine.
+3. Navigate to the application directory and run `flask run` to start the server.
+4. Access the application through `localhost:5000` in your browser.
+
+## Usage
+- The main page displays all log entries.
+- Use the search bar to filter entries based on keywords.
+- Click on a log entry to view more details.
+
+## Contributing
+Contributions to the project are welcome. Please fork the repository and submit a pull request with your changes.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
